67a7b16a5ff3546e65fc0f7e76f959595688ffd5
priddhesh <109747774+priddhesh@users.noreply.github.com>
2022-10-05 11:36:09 +0530

    Update detectLoopInLL.cpp

--- detectLoopInLL.cpp
+++ detectLoopInLL.cpp
@@ -1,1 +1,33 @@
 
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+      if(!head) return false;
+      unordered_map<Node *, int>m;
+      while(head)
+      {
+          if(!m[head])
+          {
+              m[head] = 1;
+              head = head->next;
+          }
+          else return true;
+      }
+      return false;
+    }
+};
